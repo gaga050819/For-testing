a89866180af61aeb909da580bc28b6ae2bb1847a
gaga050819 <gaga050819@gmail.com>
2026-04-09 19:18:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,13 @@
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./index.html']))
+  );
+});
+
 self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
